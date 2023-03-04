67fa041cc04a120baf3de73c737ec80fb5aca3a5
MarieJosee13 <mariejosee1313@gmail.com>
2023-03-04 08:36:22 -0800

    added local time to my first city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+function updateVancouver() {
+  let vancouverElement = document.querySelector("#vancouver");
+  let vancouverDateElement = vancouverElement.querySelector(".date");
+  let vancouverTimeElement = vancouverElement.querySelector(".time");
+  let vancouverTime = moment().tz("America/Vancouver");
 
+  vancouverDateElement.innerHTML = vancouverTime.format("dddd, MMMM Do YYYY");
+  vancouverTimeElement.innerHTML = vancouverTime.format(
+    "h:mm:ss [<small>] a [</small>]"
+  );
+}
+
+function updateParis() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("dddd, MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format(
+    "h:mm:ss [<small>] a [</small>]"
+  );
+}
+updateVancouver();
+updateParis;
+setInterval(updateVancouver, 1000);
+setInterval(updateParis, 1000);
